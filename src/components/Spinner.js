f2bcddecba208cb96a8828e18c7f04b444b229d5
Priyabrata Mohanty <86543730+primohanty@users.noreply.github.com>
2023-11-16 21:51:12 +0530

    Create Spinner.js

---
+++ src/components/Spinner.js
@@ -0,0 +1,13 @@
+import "./Spinner.css"
+
+const Spinner = () => {
+    return (
+        <div className="h-[100vh] flex justify-center items-center">
+            <div className="spinner">
+            </div>
+
+        </div>
+    )
+}
+
+export default Spinner
